b4cd0e51a167928ce70ac7362f5708414c4153d0
klept0 <github@klept0.com>
2024-06-04 08:59:51 -0400

    Update browser.py
    option should block privacy popup in Chrome

--- src/browser.py
+++ src/browser.py
@@ -73,6 +73,7 @@ class Browser:
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-default-apps")
         options.add_argument("--disable-features=Translate")
+        options.add_argument('--disable-features=PrivacySandboxSettings4')
 
         seleniumwireOptions: dict[str, Any] = {"verify_ssl": False}
 
